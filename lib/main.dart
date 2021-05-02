f0a629d11e24f44264d180c9d976bb21c8327ec4
Hashir Khan <khnhashir555@gmail.com>
2021-05-02 17:42:24 +0500

    First Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,87 @@ class HomePage extends StatelessWidget {
         title: Center(child: Text('First Flutter App')),
       ),
       body: Center(
-        child: Container(
-          child: Text('Hello World'),
+        child: Row(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              child: Text(
+                'I am a box 1',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 19,
+                ),
+              ),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey[400],
+                      blurRadius: 5,
+                      offset: Offset(2.0, 5.0),
+                    )
+                  ],
+                  gradient:
+                      LinearGradient(colors: [Colors.yellow, Colors.pink])),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              child: Text(
+                'I am a box 2',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 19,
+                ),
+              ),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey[400],
+                      blurRadius: 5,
+                      offset: Offset(2.0, 5.0),
+                    )
+                  ],
+                  gradient:
+                      LinearGradient(colors: [Colors.yellow, Colors.pink])),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              child: Text(
+                'I am a box 3',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 19,
+                ),
+              ),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey[400],
+                      blurRadius: 5,
+                      offset: Offset(2.0, 5.0),
+                    )
+                  ],
+                  gradient:
+                      LinearGradient(colors: [Colors.yellow, Colors.pink])),
+            )
+          ],
         ),
       ),
     );
